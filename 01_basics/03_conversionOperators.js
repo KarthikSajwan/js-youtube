3fd118b69bcde19d6dd13902d1d7523a34d2985e
Karthik Sajwan <114817612+KarthikSajwan@users.noreply.github.com>
2024-04-18 17:40:46 +0000

    third

--- 01_basics/03_conversionOperators.js
+++ 01_basics/03_conversionOperators.js
@@ -1,3 +1,14 @@
-let score = 33
-
+let score = "33"  // "" means it is string
 console.log(typeof score)
+
+let valueInNumber = Number(score)  // converted to integer
+console.log(typeof valueInNumber)
+
+
+
+// "33" => 33 number
+//  "33abc" => NAN number converted but value is NAN 
+
+let str1 = "karthik"
+let str2 = "sajwan"
+console.log(str3 = str1 + str2)
