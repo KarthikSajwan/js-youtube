05dac605fea50d3dbb6a4c1b70c8f79293ee74df
Karthik Sajwan <114817612+KarthikSajwan@users.noreply.github.com>
2024-04-18 17:16:46 +0000

    third

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+"use strict";  //treat all JS code as newer version
+
+// alert(3+3)  //only in browser but we are using node js
+
+console.log(3+3)
+
+console.log("Karthik")
+
+let name = "karthik"
+let age = 20
+let isLoggedin = false
+let state = null   //null or empty
+let random  //undefined
+
+console.log(typeof "karthik")
