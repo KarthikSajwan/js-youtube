c631f166e517a1c283b59dc4ee20501340906e11
Karthik Sajwan <114817612+KarthikSajwan@users.noreply.github.com>
2024-04-18 18:10:32 +0000

    third commit

---
+++ 01_basics/04_comaprison.js
@@ -0,0 +1,13 @@
+console.log("2" > 1)
+console.log("02" > 1)
+
+
+console.log("NULL comaprison:")
+console.log(null > 0)
+console.log(null == 0)
+console.log(null <= 0) // true why?
+
+
+// === also checks datatype
+
+console.log("2" === 2)
