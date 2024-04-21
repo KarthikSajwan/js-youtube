05c3c1dc3d46f7ce15da2fb37f216cd6bb391d8e
Karthik Sajwan <114817612+KarthikSajwan@users.noreply.github.com>
2024-04-21 14:57:14 +0000

    array

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,5 +1,7 @@
 // array
 
-const myArr = [0, 1, 2, 3, 4, 5, 
-    
-    console.log(myArr[1])
+const myArr = [0, 1, 2, 3, 4, 5];
+const myName = ["karthik", "sajwan"]
+
+const Arr2 = new Array (1,2,3,4)
+console.log(myArr[1]);
