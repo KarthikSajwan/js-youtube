cad2c15bb394a044b3e55d2a4da7b74c952f3670
Karthik Sajwan <114817612+KarthikSajwan@users.noreply.github.com>
2024-04-23 19:48:40 +0000

    function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -13,3 +13,22 @@ function loginUserName(userName){
     return `${userName} just logged in!`
 }
 console.log(loginUserName("Karthik"));
+
+
+//REST OPERATOR
+function clacCartPrice(...num){
+    return num
+}
+console.log(clacCartPrice(200,400,600));
+
+
+const user = {
+    userName: "Karthik",
+    price: 199
+}
+
+function handleObject(anyobject) {
+    console.log(`Username is ${anyobject.userName} and price is ${anyobject.price}`);
+}
+
+handleObject(user)
