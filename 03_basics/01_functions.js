50321cc2ca461e9a6b898b5fe812528098215547
Karthik Sajwan <114817612+KarthikSajwan@users.noreply.github.com>
2024-04-23 15:42:36 +0000

    function

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,15 @@
+function sayMyName(){
+    console.log("Walter White");
+}
+
+sayMyName()
+
+function addNumbers(num1, num2) {
+    console.log(num1+num2);
+}
+addNumbers(2,3)
+
+function loginUserName(userName){
+    return `${userName} just logged in!`
+}
+console.log(loginUserName("Karthik"));
