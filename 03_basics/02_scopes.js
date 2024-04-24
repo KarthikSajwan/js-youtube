93b7e55302a7f74882b79840877af6016af2d961
Karthik Sajwan <114817612+KarthikSajwan@users.noreply.github.com>
2024-04-24 09:20:10 +0000

    function hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -5,3 +5,35 @@ if (true) {
     var c = 30; //this will be printed outside which is a VAR problem
 }
 
+function one() {
+    const username = "Karthik"
+
+    function two(){
+        const website = "YouTube"
+        console.log(username);
+    }
+    //console.log(website);     // chil can ask for icecream from parent but parent cant ask from child
+    
+    two()
+}
+//one()
+
+if (true) {
+    const username = "Karthik"
+    if (username === "Karthik") {
+        const web = "Youtube"
+        console.log(username + web);
+    }
+}
+
+//++++++++++++++++++++++INTERESTING(HOISTING concept)+++++++++++++++++++++++++++
+
+console.log(addone(5));  // can be accessed anywhere 
+function addone(num){
+    return num+1
+}
+
+const addTwo = function(num){
+    return num+2
+}
+console.log(addTwo(5));  // only accessible after the fun decalrationn as it is stored in const
