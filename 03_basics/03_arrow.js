f8795765d62cad0ae3fcd4660d397ea5c53f52a0
Karthik Sajwan <114817612+KarthikSajwan@users.noreply.github.com>
2024-04-24 13:52:36 +0000

    this and arrow

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -9,3 +9,29 @@ const user = {
 user.welcomeMsg()
 user.username = "Sajwan"   //username will be updated as we used this and it refers to current context not hard coded
 user.welcomeMsg()
+
+
+//This will be undefined
+// function chai(){
+//     let username = "Karthik"
+//     console.log(this.username);
+// }
+// chai()
+
+const chai = () => {
+    let username = "Karthik"
+    console.log(this);
+}
+
+//+++++++++++++++++++(ARROW FUNCTION)++++++++++++++++++++++++
+
+//EXPLICIT RETURN
+const addTwo = (num1, num2) => {
+    return num1 + num2
+}
+console.log(addTwo(3,4))
+
+//IMPLICIT RETURN METHOD
+const add = (num1, num2) => num1 + num2
+console.log(add(2,2));
+
