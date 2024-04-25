91b83b4e97588fa6e215a016d81a271177d5fa86
Karthik Sajwan <114817612+KarthikSajwan@users.noreply.github.com>
2024-04-25 17:52:33 +0000

    if

---
+++ 04_control_flow/one.js
@@ -0,0 +1,10 @@
+const balance = 500
+
+if (balance > 500) {
+    console.log("less than");
+} else if (balance < 750) {
+    console.log("less than 750");
+} else{
+    console.log("less than 2000");
+}
+
