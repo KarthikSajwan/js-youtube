9bb032831794fa1070a5134916ff5eb6810053b4
Karthik Sajwan <114817612+KarthikSajwan@users.noreply.github.com>
2024-04-30 18:03:35 +0000

    array loops and map

---
+++ 05_iterations/three.js
@@ -0,0 +1,14 @@
+const arr = [1, 2, 3, 4, 5]
+
+for( const num of arr) {
+    console.log(num);
+} 
+
+const grreetings = "Hello!"
+for (const greet of grreetings){
+    console.log(`Each char is ${greet}`);
+}
+
+//Maps
+const map = new Map()
+map.
